b3c50a04d2f2a9916ba9c0c56fb5af6889fef8b3
Maximilian <derivas121@gmail.com>
2020-04-24 17:27:08 +0300

    Create 144Bin_Tree_inorder.py

---
+++ 144Bin_Tree_inorder.py
@@ -0,0 +1,21 @@
+#Inorder: node,node.left,node.right
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        stack = [root]
+        preorder = []
+        while stack:
+            node = stack.pop()
+            preorder.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return preorder
